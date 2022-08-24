d8b048f2435b73d6cc484bfd3212b1e4685361ea
Egor Dudyrev <egor.dudyrev@yandex.ru>
2022-08-24 18:12:56 +0300

    Add MetaData class

--- benches/datahandler.py
+++ benches/datahandler.py
@@ -1,13 +1,20 @@
 """This file describes DataHandler class that keeps track of all datasets and where they can be loaded from"""
-from typing import Tuple
+from dataclasses import dataclass
+from typing import Tuple, Optional, List
 from urllib import request
 
 import pandas as pd
-import os
 
 
-TYPE_DATA_AND_META = Tuple[pd.DataFrame, dict]  # First is dataframe with data itself, second is its metadata
+@dataclass
+class MetaData:
+    name: str
+    url: str
+    x_feats: List[str]
+    y_feats: Optional[List[str]] = None
 
+
+TYPE_DATA_AND_META = Tuple[pd.DataFrame, MetaData]
 
 
 class DataHandler:
@@ -22,6 +29,16 @@ class DataHandler:
             tealady=lambda: self._load_cxt_from_fcapy('tealady')
         )
 
+        self._raw_url_selector = dict(
+            animal_movement='https://raw.githubusercontent.com/EgorDudyrev/FCApy/main/data/animal_movement.csv',
+            mango='https://raw.githubusercontent.com/EgorDudyrev/FCApy/main/data/mango.csv',
+            live_in_water='https://raw.githubusercontent.com/EgorDudyrev/FCApy/main/data/liveinwater.cxt',
+            digits='https://raw.githubusercontent.com/EgorDudyrev/FCApy/main/data/digits.cxt',
+            gewaesser='https://raw.githubusercontent.com/EgorDudyrev/FCApy/main/data/gewaesser.cxt',
+            lattice='https://raw.githubusercontent.com/EgorDudyrev/FCApy/main/data/lattice.cxt',
+            tealady='https://raw.githubusercontent.com/EgorDudyrev/FCApy/main/data/tealady.cxt',
+        )
+
     @property
     def available_datasets(self):
         return tuple(self._func_selector.keys())
@@ -30,26 +47,17 @@ class DataHandler:
         df_meta = self._func_selector[data_name]()
         return df_meta
 
-    @staticmethod
-    def _load_csv_from_fcapy(data_name: str) -> TYPE_DATA_AND_META:
-        raw_url_selector = {
-            'animal_movement': 'https://raw.githubusercontent.com/EgorDudyrev/FCApy/main/data/animal_movement.csv',
-            'mango': 'https://raw.githubusercontent.com/EgorDudyrev/FCApy/main/data/mango.csv',
-        }
-        df = pd.read_csv(raw_url_selector[data_name], index_col=0)
-        return df, {}
+    def _load_csv_from_fcapy(self, data_name: str) -> TYPE_DATA_AND_META:
+        url = self._raw_url_selector[data_name]
+        df = pd.read_csv(url, index_col=0)
 
-    @staticmethod
-    def _load_cxt_from_fcapy(data_name: str) -> TYPE_DATA_AND_META:
+        meta = MetaData(name=data_name, url=url, x_feats=list(df.columns))
+        return df, meta
+
+    def _load_cxt_from_fcapy(self, data_name: str) -> TYPE_DATA_AND_META:
         from fcapy.context import FormalContext
-
-        raw_url_selector = {
-            'live_in_water': 'https://raw.githubusercontent.com/EgorDudyrev/FCApy/main/data/liveinwater.cxt',
-            'digits': 'https://raw.githubusercontent.com/EgorDudyrev/FCApy/main/data/digits.cxt',
-            'gewaesser': 'https://raw.githubusercontent.com/EgorDudyrev/FCApy/main/data/gewaesser.cxt',
-            'lattice': 'https://raw.githubusercontent.com/EgorDudyrev/FCApy/main/data/lattice.cxt',
-            'tealady': 'https://raw.githubusercontent.com/EgorDudyrev/FCApy/main/data/tealady.cxt',
-        }
-        response = request.urlopen(raw_url_selector[data_name])
+        url = self._raw_url_selector[data_name]
+        response = request.urlopen(url)
         df = FormalContext.read_cxt(data=response.read().decode()).to_pandas()
-        return df, {}
+        meta = MetaData(name=data_name, url=url, x_feats=list(df.columns))
+        return df, meta
